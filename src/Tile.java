b520a69617ed9c039e2f17683f27d3f312d6bc42
codingInSpace <jonathan.grangien@gmail.com>
2017-03-31 14:05:43 +0200

    Initial commit

---
+++ src/Tile.java
@@ -0,0 +1,12 @@
+
+public class Tile {
+    private Integer Number;
+
+    public void setNumber(Integer number) {
+        Number = number;
+    }
+
+    public Integer getNumber() {
+        return Number;
+    }
+}
